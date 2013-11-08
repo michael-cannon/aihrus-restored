024eea601f81bc372dce39ff8c2a78d2abbd008b
Michael Cannon <mc@aihr.us>
2013-11-08 13:03:03 -0800

    Remove GA inclusion

--- aihrus/custom_functions.php
+++ aihrus/custom_functions.php
@@ -104,20 +104,6 @@ function aihrus_wp_footer() {
 	}
 </script>
 <!-- end Zendesk support -->
-
-<!-- Google Analytics Code -->
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-20956818-1']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
-<!-- end Google Analytics Code -->
 EOD;
 }
 add_action( 'wp_footer', 'aihrus_wp_footer', 20 );
