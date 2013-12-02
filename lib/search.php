a96bc92b008da425639416eafdc576c2391e9ec6
Michael Cannon <mc@aihr.us>
2013-12-01 17:14:38 -0800

    Include all post types

--- lib/search.php
+++ lib/search.php
@@ -8,12 +8,19 @@
 // add_filter( 'pre_get_posts', 'aihrus_search_all_post_types' );
 function aihrus_search_all_post_types( $query ) {
 	if ( $query->is_search ) {
-		$args = array(
+		$args        = array(
 			'public' => true,
 			'_builtin' => true,
 		);
+		$pt_built_in = get_post_types( $args );
 
-		$post_types = get_post_types( $args );
+		$args      = array(
+			'public' => true,
+			'_builtin' => false,
+		);
+		$pt_custom = get_post_types( $args );
+		
+		$post_types = array_merge( $pt_custom, $pt_built_in );
 		$query->set( 'post_type', $post_types );
 	}
 
