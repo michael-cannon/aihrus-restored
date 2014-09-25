6d4085dcfcfdcb76f04c681f57ea4b1ba863eb13
Michael Cannon <mc@aihr.us>
2014-09-25 21:22:53 +0200

    Enable updating checks

--- custom/aihrus/custom_functions.php
+++ custom/aihrus/custom_functions.php
@@ -370,8 +370,10 @@ $func = function ($a) {
 		'version_checked' => $wp_version,
 	);
 };
-add_filter( 'pre_site_transient_update_core', $func );
-add_filter( 'pre_site_transient_update_plugins', $func );
-add_filter( 'pre_site_transient_update_themes', $func );
+// add_filter( 'pre_site_transient_update_core', $func );
+// add_filter( 'pre_site_transient_update_plugins', $func );
+// add_filter( 'pre_site_transient_update_themes', $func );
+
+unregister_widget('featured_user_widget');
 
 ?>
